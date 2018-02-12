0f14752c38c5361a4fe8569d9920c3ea73861a3b
MiguelNdeCarvalho <miguelndecarvalho@gmail.com>
2018-02-12 14:47:11 +0000

    msm8956-common: Add fingerprint hal
    Change-Id: I588125929e5995702d8257c350758835391ef851

--- msm8956.mk
+++ msm8956.mk
@@ -159,8 +159,9 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.0-service-custom
-
+    android.hardware.biometrics.fingerprint@2.0-service-custom \
+    fingerprintd
+ 
 # FM
 PRODUCT_PACKAGES += \
     FMRadio \
