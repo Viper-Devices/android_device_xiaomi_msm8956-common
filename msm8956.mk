069b2121c679cc61b0e7373a27d1d8550561d990
kessaras <theodoroupaulos@gmail.com>
2018-02-12 17:43:03 +0200

    msm8956-common: Add missing RIL hals
    Change-Id: I46cce2069617956bd3883c1a4ee4210864d56533

--- msm8956.mk
+++ msm8956.mk
@@ -338,3 +338,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/p2p_supplicant_overlay.conf:system/etc/wifi/p2p_supplicant_overlay.conf \
     $(LOCAL_PATH)/wifi/wpa_supplicant_overlay.conf:system/etc/wifi/wpa_supplicant_overlay.conf
+
+# RIL
+PRODUCT_PACKAGES += \
+    librmnetctl \
+    libxml2
